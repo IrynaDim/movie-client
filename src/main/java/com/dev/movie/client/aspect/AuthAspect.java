29777f912d7c8ff02bde5703895861c62c20142e
i.dymytreyeva <i.dymytreyeva@gmail.com>
2021-06-14 17:10:40 +0300

    added AuthAspect

--- src/main/java/com/dev/movie/client/aspect/AuthAspect.java
+++ src/main/java/com/dev/movie/client/aspect/AuthAspect.java
@@ -1,6 +1,8 @@
 package com.dev.movie.client.aspect;
 
+import com.dev.movie.client.entity.TokenStorage;
 import com.dev.movie.client.exception.UnauthorizedException;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,19 +11,21 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class AuthAspect {
 
-    @Pointcut("within(com.dev.movie.client.service.CinemaHallService))")
-    public void cinemaHallMethod() {
-    }
-
-    @Pointcut("within(com.dev.movie.client.service.MovieService)")
+    @Pointcut("execution(* com.dev.movie.client.service.ClientService.findAllHall(..)))")
     public void movieMethod() {
     }
 
-    @Before("cinemaHallMethod() || movieMethod()")
+    @Pointcut("execution(* com.dev.movie.client.service.ClientService.findAllMovies(..)))")
+    public void cinemaHallMethod() {
+    }
+
+    @Before("(cinemaHallMethod() || movieMethod()) && !@annotation(com.dev.movie.client.config.AllUser)")
     public void checkUserToken(JoinPoint joinPoint) {
         Object[] values = joinPoint.getArgs();
-        if (values[0].equals("")) {
-            throw new UnauthorizedException();
+        for (Object value : values) {
+            if (TokenStorage.class.equals(value.getClass()) && ((TokenStorage) value).getToken() == null) {
+                throw new UnauthorizedException();
+            }
         }
     }
 }
