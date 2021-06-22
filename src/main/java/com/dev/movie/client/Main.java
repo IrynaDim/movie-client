c645b30b67b3ad71a30a50e56fb24523cfb37b64
i.dymytreyeva <i.dymytreyeva@gmail.com>
2021-06-22 13:41:31 +0300

    added small fix

--- src/main/java/com/dev/movie/client/Main.java
+++ src/main/java/com/dev/movie/client/Main.java
@@ -3,7 +3,6 @@ package com.dev.movie.client;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
